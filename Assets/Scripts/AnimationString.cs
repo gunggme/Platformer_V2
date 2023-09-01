28aea3c02c0343fa4ce3aa076f0be725f29c4ec4
gunggme <ekfrifajfl2019@gmail.com>
2023-09-01 19:14:07 +0900

    Feat : 공격 판정 완성

---
+++ Assets/Scripts/AnimationString.cs
@@ -0,0 +1,7 @@
+﻿public class AnimationString
+{
+    internal static string moveBool = "move";
+    internal static string isGroundBool = "isGround";
+    internal static string jumpTrigger = "jump";
+    internal static string attackTrigger = "attack";
+}
